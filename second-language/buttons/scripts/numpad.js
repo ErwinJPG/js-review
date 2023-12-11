ade108306c722f39be7f6195fab1333bef4d805f
Erwin Maximus Panergo <e.panergo@EDU.epsb.ca>
2023-12-11 10:18:14 -0700

    .

--- second-language/buttons/scripts/numpad.js
+++ second-language/buttons/scripts/numpad.js
@@ -4,8 +4,14 @@ numpad_ref.addEventListener("click", play_dial);
 var soundPlayer = new Audio();
 
 function play_dial() {
+    // https://stackoverflow.com/questions/25157513/javascript-pitch-shift-with-time-stretch
+    // https://stackoverflow.com/questions/18826147/javascript-audio-play-on-click
+    // dialing "3-3-2-3-9-3" should sound like funky town
     soundPlayer.src = "sounds/piano.mp3"; // <-- Find audio
     soundPlayer.mozPreservesPitch = false;
     soundPlayer.playbackRate = 0.5;
     soundPlayer.play();
+}
+
+function funk() {
 }
