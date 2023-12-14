ff2c4afa4c29d4ee5c3b99268cd16c577fce2d75
Erwin Maximus Panergo <e.panergo@EDU.epsb.ca>
2023-12-14 10:16:42 -0700

    .

--- second-language/buttons/scripts/numpad.js
+++ second-language/buttons/scripts/numpad.js
@@ -1,5 +1,6 @@
 var soundPlayer = new Audio();
 var hits = 0;
+var rainbowed = false;
 
 function play_dial(num) {
     // https://stackoverflow.com/questions/25157513/javascript-pitch-shift-with-time-stretch
@@ -18,10 +19,24 @@ function play_dial(num) {
 
 function funk(num) {
     let funky_tones = [5,5,7,5,4,4,5,0,8,5];
-    if (num == funky_tones[hits]) {
+    if (num == funky_tones[hits] && !rainbowed) {
         hits += 1;
     } else {
         hits = 0;
     };
-    console.log(hits);
+
+    if (hits >= funky_tones.length) {
+        rainbow_mode();
+        rainbowed = true;
+    }
+    // https://stackoverflow.com/questions/7171483/simple-way-to-get-element-by-id-within-a-div-tag
+    // https://stackoverflow.com/questions/4019894/get-all-li-elements-in-array
+}
+
+function rainbow_mode() {
+    console.log("!!");
+    let buttons = document.getElementById("keyboard").getElementsByTagName("li");
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].className = buttons[i].className + " funk";
+    }
 }
