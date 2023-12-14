e228769029e25930e3e3d36b9e8e9b48bf287677
Erwin Maximus Panergo <e.panergo@EDU.epsb.ca>
2023-12-14 11:33:45 -0700

    .

--- second-language/buttons/scripts/numpad.js
+++ second-language/buttons/scripts/numpad.js
@@ -6,11 +6,12 @@ function play_dial(num) {
     // https://stackoverflow.com/questions/25157513/javascript-pitch-shift-with-time-stretch
     // https://stackoverflow.com/questions/18826147/javascript-audio-play-on-click
     // dialing "3-3-2-3-9-3" should sound like funky town
-    if (num < 0 || num > 9) {return};
-    let cmd = `sounds/dialpad/${num}.mp3`;
+    // Should preload audio at DOM loading
+    if (num < 0 || num > 9) { return };
+    let file = `sounds/dialpad/${num}.mp3`;
 
-    soundPlayer.src = cmd; // <-- Find audio
-    soundPlayer.load(cmd)
+    soundPlayer.src = file; // <-- Find audio
+    soundPlayer.load(file)
     soundPlayer.preservesPitch = false;
     soundPlayer.playbackRate = 1;
     soundPlayer.play();
@@ -18,9 +19,8 @@ function play_dial(num) {
 }
 
 function funk(num) {
-    // DISABLE TEXT HIGHLIGHTING FOR BUTTONS
     // https://www.w3schools.com/howto/howto_css_disable_text_selection.asp
-    let funky_tones = [5,5,7,5,4,4,5,0,8,5];
+    let funky_tones = [5, 5, 7, 5, 4, 4, 5, 0, 8, 5];
     if (num == funky_tones[hits] && !rainbowed) {
         hits += 1;
     } else {
@@ -31,11 +31,11 @@ function funk(num) {
         rainbow_mode();
         rainbowed = true;
     }
-    // https://stackoverflow.com/questions/7171483/simple-way-to-get-element-by-id-within-a-div-tag
-    // https://stackoverflow.com/questions/4019894/get-all-li-elements-in-array
 }
 
 function rainbow_mode() {
+    // Appends "funk" at end of class name to start animation
+    // How to make effect persist after tab change?
     console.log("!!");
     let buttons = document.getElementById("keyboard").getElementsByTagName("li");
     for (let i = 0; i < buttons.length; i++) {
