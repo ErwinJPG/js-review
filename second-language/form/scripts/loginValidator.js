cde1e587f2939bb099509adb2e1b22dbbd4298d8
Erwin Maximus Panergo <e.panergo@EDU.epsb.ca>
2023-12-15 11:19:19 -0700

    add form assignment

---
+++ second-language/form/scripts/loginValidator.js
@@ -0,0 +1,13 @@
+// https://www.w3schools.com/js/js_validation.asp
+function validate_form() {   
+    let email = document.forms["login-form"]["EmailForm"]["email"];
+    let pass = document.forms["login-form"]["PassForm"]["password"];
+    alert(email);
+    alert(pass);
+}
+function validate_email() {   
+
+}
+function validate_pass() {   
+
+}
