c99412028cd9c4bd1701ebfb65b74af27e88bf5a
Erwin Maximus Panergo <e.panergo@EDU.epsb.ca>
2023-12-15 11:37:51 -0700

    .

--- second-language/form/scripts/loginValidator.js
+++ second-language/form/scripts/loginValidator.js
@@ -1,13 +1,25 @@
 // https://www.w3schools.com/js/js_validation.asp
+
+const regex_email = /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
+
 function validate_form() {   
-    let email = document.forms["login-form"]["EmailForm"]["email"];
-    let pass = document.forms["login-form"]["PassForm"]["password"];
-    alert(email);
-    alert(pass);
+    let email = document.getElementById("EmailField").value;
+    let pass = document.getElementById("PassField").value;
+    validate_email(email);
+    validate_pass(pass);
 }
-function validate_email() {   
 
+// Add ability to turn bottom border of input red when incorrect
+function validate_email(email) {   
+    let x = email.match(regex_email);
+    if (x == null) {
+        alert("Invalid email");
+    } else {
+        console.log(email);
+    }
 }
-function validate_pass() {   
 
+function validate_pass(pass) {  
+    //console.log(pass);
+    //alert(pass);
 }
