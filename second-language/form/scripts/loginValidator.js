13a373c910ddf2aaa04a3f1cadf7b4021ff25f0b
Your name <kengpanergo@gmail.com>
2023-12-16 13:51:14 -0700

    Incomplete:     - Error cross does not appear after incorrect email input     - Background does not appear         - Appears when login-panel position attribute is changed         - tf     - Back button does not work

--- second-language/form/scripts/loginValidator.js
+++ second-language/form/scripts/loginValidator.js
@@ -1,13 +1,7 @@
 // https://www.w3schools.com/js/js_validation.asp
 
 const regex_email = /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
-
-function validate_form() {
-    let email = document.getElementById("EmailField").value;
-    let pass = document.getElementById("PassField").value;
-    validate_email(email);
-    validate_pass(pass);
-}
+function validate_form() {}
 
 // Add ability to turn bottom border of input red when incorrect
 function validate_email() {
@@ -20,12 +14,25 @@ function validate_email() {
     // Implement using element.classList.add("incorrect-email") and .remove(...) 
     if (x == null) {
         console.log("Invalid email");
+        element.classList.add("invalid-email");
+        element.after(create_cross());
     } else {
         console.log(email);
     }
 }
 
-function validate_pass(pass) {
-    //console.log(pass);
-    //alert(pass);
+function create_cross() {
+    const svg = document.createElementNS("", "svg");
+    svg.classList.add("error-class");
+    
+    const path1 = document.createElementNS("", "path");
+    path1.setAttribute("d", 'M0,0 L18,18');
+    path1.setAttribute('class', 'error-cross1');
+    const path2 = document.createElementNS("", "path");
+    path2.setAttribute("d", "M18, 0 L0,18" );
+    path2.setAttribute('class', 'error-cross2');
+    svg.append(path1);
+    svg.append(path2);
+
+    return svg
 }
