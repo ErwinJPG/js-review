a4af2e805cb61f0f562a253a755b84ca1391bc83
Your name <kengpanergo@gmail.com>
2023-12-15 23:49:12 -0700

    Needs to warn user of invalid inputted email.

--- second-language/form/scripts/loginValidator.js
+++ second-language/form/scripts/loginValidator.js
@@ -2,7 +2,7 @@
 
 const regex_email = /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
 
-function validate_form() {   
+function validate_form() {
     let email = document.getElementById("EmailField").value;
     let pass = document.getElementById("PassField").value;
     validate_email(email);
@@ -10,16 +10,22 @@ function validate_form() {
 }
 
 // Add ability to turn bottom border of input red when incorrect
-function validate_email(email) {   
+function validate_email() {
+    let element = document.getElementById("EmailField");
+    let email = element.value;
     let x = email.match(regex_email);
+    // Should show user that the input is not a valid email by making bottom bar red,
+    // Append an animated X to side of email form,
+    // Create a text (label) displaying "Invalid email."
+    // Implement using element.classList.add("incorrect-email") and .remove(...) 
     if (x == null) {
-        alert("Invalid email");
+        console.log("Invalid email");
     } else {
         console.log(email);
     }
 }
 
-function validate_pass(pass) {  
+function validate_pass(pass) {
     //console.log(pass);
     //alert(pass);
 }
