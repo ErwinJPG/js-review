2defba674691e22bad3a26e313df2458e0c460c7
Your name <kengpanergo@gmail.com>
2023-12-16 16:58:05 -0700

    LETS GOOOOO

--- second-language/form/scripts/loginValidator.js
+++ second-language/form/scripts/loginValidator.js
@@ -1,68 +1,66 @@
-// https://www.w3schools.com/js/js_validation.asp
+const emailRegex = /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
+let isInvalidEmail = false;
+let isFieldInFocus = false;
+let emailField = document.getElementById("EmailField");
 
-const regex_email = /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
-var is_invalid = false;
-var in_focus = false;
-var email_field = document.getElementById("EmailField");
-if (email_field) {
-    email_field.addEventListener('focus', function() {
-        in_focus = true;
-    })
-    email_field.addEventListener('focusout', function() {
-        in_focus = false;
-    })
+if (emailField) {
+    emailField.addEventListener('focus', function() {
+        isFieldInFocus = true;
+    });
+
+    emailField.addEventListener('focusout', function() {
+        isFieldInFocus = false;
+        validateEmail();
+    });
 }
 
-// Add ability to turn bottom border of input red when incorrect
-function validate_email() {
-    let email = email_field.value;
-    let x = email.match(regex_email);
-    let svg = create_cross()
-    let p = create_requirements()
-    // Should show user that the input is not a valid email by making bottom bar red,
-    // Append an animated X to side of email form,
-    // Create a text (label) displaying "Invalid email."
-    // Implement using email_field.classList.add("incorrect-email") and .remove(...) 
-    if (x == null && !is_invalid) {
-        is_invalid = true;
-        email_field.classList.add("invalid-email-border");
-        email_field.after(p);
-        email_field.after(svg);
-    } 
-    if (x != null) {
-        is_invalid = false;
-        email_field.classList.remove("invalid-email-border");
-        document.getElementById("error-div").remove(); // SPAMS CONSOLE WITH ERROR.
-        document.getElementById("invalid-message").remove(); // SPAMS CONSOLE WITH ERROR.
+function validateEmail() {
+    let emailValue = emailField.value;
+    let isValidEmail = emailValue.match(emailRegex);
+    let errorDiv = document.getElementById("error-div");
+    let invalidMessage = document.getElementById("invalid-message");
+
+    if (isValidEmail === null && !isInvalidEmail && !isFieldInFocus) {
+        isInvalidEmail = true;
+        emailField.classList.add("invalid-email-border");
+        emailField.after(createRequirements());
+        emailField.after(createCross());
+    } else if (isValidEmail !== null) {
+        isInvalidEmail = false;
+        emailField.classList.remove("invalid-email-border");
+        if (errorDiv) { errorDiv.remove(); }
+        if (invalidMessage) { invalidMessage.remove(); }
     }
-    console.log(x, is_invalid, in_focus)
 }
 
-function create_cross() {
+function createCross() {
     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     svg.setAttribute("id", "error-div");
     svg.classList.add("error-class");
-    
+
     const path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
     path1.setAttribute("d", 'M0,0 L18,18');
     path1.setAttribute('class', 'error-cross1');
+    
     const path2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
     path2.setAttribute("d", "M18, 0 L0,18" );
     path2.setAttribute('class', 'error-cross2');
+    
     svg.append(path1);
     svg.append(path2);
 
-    return svg
+    return svg;
 }
 
-//terrible naming
-function create_requirements() {
+function createRequirements() {
     const p = document.createElement("p");
     const text = document.createTextNode("Invalid email.");
-    p.setAttribute("class", "invalid-email")
-    p.setAttribute("id", "invalid-message")
-    p.appendChild(text)
-    return p
+    p.setAttribute("class", "invalid-email");
+    p.setAttribute("id", "invalid-message");
+    p.appendChild(text);
+    return p;
 }
 
-function validate_form() {}
+function validateForm() {
+    // Implementation for form validation if needed
+}
