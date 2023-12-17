2bc3e04366dac0564c6bffe08a4342037e8e5ac2
Your name <kengpanergo@gmail.com>
2023-12-16 18:37:45 -0700

    gyat

--- second-language/form/scripts/loginValidator.js
+++ second-language/form/scripts/loginValidator.js
@@ -1,7 +1,9 @@
 const emailRegex = /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
-let isInvalidEmail = false;
-let isFieldInFocus = false;
-let emailField = document.getElementById("EmailField");
+const userRegex = /^(?=.$)[a-zA-Z0-9._]/i; // https://stackoverflow.com/questions/12018245/regular-expression-to-validate-username
+var isInvalidEmail = false;
+var isInvalidUsername = false;
+var isFieldInFocus = false;
+var emailField = document.getElementById("EmailField");
 
 if (emailField) {
     emailField.addEventListener('focus', function() {
@@ -17,17 +19,23 @@ if (emailField) {
 function validateEmail() {
     let emailValue = emailField.value;
     let isValidEmail = emailValue.match(emailRegex);
-    let errorDiv = document.getElementById("error-div");
-    let invalidMessage = document.getElementById("invalid-message");
+
+    var errorDiv = document.getElementById("error-div");
+    var invalidMessage = document.getElementById("invalid-message");
+    var checkDiv = document.getElementById("check-div");
 
     if (isValidEmail === null && !isInvalidEmail && !isFieldInFocus) {
         isInvalidEmail = true;
+        emailField.classList.remove("valid-email-border")
         emailField.classList.add("invalid-email-border");
-        emailField.after(createRequirements());
-        emailField.after(createCross());
+        if (!errorDiv) emailField.after(createRequirements());
+        if (!errorDiv) emailField.after(createCross());
+        if (checkDiv) { checkDiv.remove(); }
     } else if (isValidEmail !== null) {
         isInvalidEmail = false;
         emailField.classList.remove("invalid-email-border");
+        emailField.classList.add("valid-email-border");
+        if (!checkDiv) emailField.after(createCheck());
         if (errorDiv) { errorDiv.remove(); }
         if (invalidMessage) { invalidMessage.remove(); }
     }
@@ -52,6 +60,23 @@ function createCross() {
     return svg;
 }
 
+function createCheck() {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("id", "check-div");
+    svg.classList.add("valid-class");
+
+    const path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path1.setAttribute("d", 'M0,9 L7,18, L18,0');
+    path1.setAttribute('class', 'check1');
+    
+    svg.append(path1);
+
+    return svg; 
+}
+
+function validateUsername() {
+}
+
 function createRequirements() {
     const p = document.createElement("p");
     const text = document.createTextNode("Invalid email.");
@@ -63,4 +88,10 @@ function createRequirements() {
 
 function validateForm() {
     // Implementation for form validation if needed
+}
+
+function nerd() {
+    console.log("nerd");
+    console.log(document.body.style.backgroundImage);
+    document.body.style.backgroundImage = "url(https://media1.tenor.com/m/xCc58fEqFREAAAAd/nerd-nerdy.gif)";
 }
