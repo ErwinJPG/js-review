d275bf73bf51f63e8e52e902fa4f1c3921d61471
ErwinJPG <e.panergo@share.epsb.ca>
2023-12-22 11:00:10 -0700

    complete products

--- second-language/product/db/products.js
+++ second-language/product/db/products.js
@@ -1,24 +1,10 @@
 var products = [
         {
-            "name": "Audeze LCD-2",
-            "description": "Best use of magnets over MRIs.",
-            "model_number": 0,
-            "image_url": "images/lcd2.webp",
-            "price": 995.00
-        },
-        {
-            "name": "Sennheiser HD 800 S",
-            "description": "Costs more than a Civic.",
-            "model_number": 1,
-            "image_url": "images/senn.jpg",
-            "price": 2399.95
-        },
-        {
-            "name": "Honda Civic 2006",
-            "description": "Includes catless downpipe.",
+            "name": "beyerdynamic DT 990 Pro",
+            "description": "Entry-level audiophile headphones",
             "model_number": 2,
-            "image_url": "images/civic.webp",
-            "price": 2000
+            "image_url": "images/beyer.jpg",
+            "price": 230
         },
         {
             "name": "Sennheiser HD 599",
@@ -28,17 +14,31 @@ var products = [
             "price": 259.95
         },
         {
+            "name": "Audeze LCD-2",
+            "description": "Best use of magnets over MRIs.",
+            "model_number": 0,
+            "image_url": "images/lcd2.webp",
+            "price": 995.00
+        },
+        {
+            "name": "BEOPLAY H95 FERRARI EDITION",
+            "description": "Designer headphones : /",
+            "model_number": 5,
+            "image_url": "images/bang.webp",
+            "price": 1799
+        },
+        {
+            "name": "Sennheiser HD 800 S",
+            "description": "Costs more than a Civic.",
+            "model_number": 1,
+            "image_url": "images/senn.jpg",
+            "price": 2399.95
+        },
+        {
             "name": "Hifiman SUSVARA",
-            "description": "Item5 description",
+            "description": "Holy grail of headphones.",
             "model_number": 4,
             "image_url": "images/sus.jpg",
             "price": 6000
-        },
-        {
-            "name": "Item6",
-            "description": "Item6 description",
-            "model_number": 5,
-            "image_url": "https://placehold.co/200x200",
-            "price": 300
         }
     ]
