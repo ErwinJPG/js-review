7437280190c228afcb03a7553e08ca461ac7e55f
ErwinJPG <e.panergo@share.epsb.ca>
2024-01-15 19:56:29 -0700

    cleanup p1

--- second-language/product-php/js/products.js
+++ second-language/product-php/js/products.js
@@ -1,70 +1,70 @@
 var products = [
         {
             "name": "beyerdynamic DT 990 Pro",
-            "model_number": 2,
+            "model_number": 0,
             "price": 230,
             "image_url": "images/beyer.png",
             "description": "Entry-level audiophile headphones"
         },
         {
             "name": "Sennheiser HD 599",
-            "model_number": 3,
+            "model_number": 1,
             "price": 259.95,
             "image_url": "images/hd599.png",
             "description": "I love this thing."
         },
         {
             "name": "Audeze LCD-2",
-            "model_number": 0,
+            "model_number": 2,
             "price": 995.00,
             "image_url": "images/lcd2.png",
             "description": "Best use of magnets over MRIs."
         },
         {
             "name": "BEOPLAY H95 FERRARI EDITION",
-            "model_number": 5,
+            "model_number": 3,
             "price": 1799,
             "image_url": "images/bang.png",
             "description": "Designer headphones : /"
         },
         {
             "name": "Sennheiser HD 800 S",
-            "model_number": 1,
+            "model_number": 4,
             "price": 2399.95,
             "image_url": "images/senn.png",
             "description": "Costs more than a Civic."
         },
         {
             "name": "Hifiman SUSVARA",
-            "model_number": 4,
+            "model_number": 5,
             "price": 6000,
             "image_url": "images/sus.png",
             "description": "Holy grail of headphones."
         },
         {
             "name": "Neumann KH 870 G",
-            "model_number": 5,
+            "model_number": 6,
             "price": 6100,
             "image_url": "images/neumann.png",
             "description": "7.1 analog bass managed subwoofer."
         },
         {
             "name": "final LAB II",
-            "model_number": 6,
+            "model_number": 7,
             "price": 10000,
             "image_url": "images/lab.png",
             "description": "Violation of your ear canals."
         },
         {
             "name": "Bachelor of Science in Electrical Engineering",
-            "model_number": 6,
+            "model_number": 8,
             "price": 11907.53,
             "image_url": "images/diploma.png",
             "description": "Ultimate path of the audiophile."
         },
         {
             "name": "Sennheiser HE-1",
-            "model_number": 7,
+            "model_number": 9,
             "price": 59000,
             "image_url": "images/he1.png",
             "description": "Reshaping excellence."
